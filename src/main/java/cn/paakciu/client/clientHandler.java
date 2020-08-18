02118518cfd97c7bb1629e5ec2c7004fcc773a3c
paakciu <asd649433749@qq.com>
2020-08-18 18:02:50 +0800

    v0.0.2-temp

---
+++ src/main/java/cn/paakciu/client/clientHandler.java
@@ -0,0 +1,7 @@
+package cn.paakciu.client;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class clientHandler extends ChannelInboundHandlerAdapter {
+    
+}
--- src/main/java/cn/paakciu/client/netty_client.java
+++ src/main/java/cn/paakciu/client/netty_client.java
@@ -9,14 +9,20 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.AttributeKey;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 
 public class netty_client {
+    private static final int MAX_RETRY = 5;
+
     public static void main(String[] args)
     {
-
+        //线程组
         NioEventLoopGroup workerGroup =new NioEventLoopGroup();
-
+        //引导类
         Bootstrap bootstrap =new Bootstrap();
+        //核心配置
         bootstrap
                 //指定线程模型
                 .group(workerGroup)
@@ -24,11 +30,14 @@ public class netty_client {
                 .channel(NioSocketChannel.class)
                 //指定IO的处理逻辑
                 .handler(new ChannelInitializer<SocketChannel>() {
+                    //连接初始化
+                    @Override
                     protected void initChannel(SocketChannel socketChannel) throws Exception {
-
+                        socketChannel.pipeline().addLast(new clientHandler());
                     }
                 });
 
+        //额外的配置
         bootstrap
                 // 绑定自定义属性到 channel
                 .attr(AttributeKey.newInstance("clientName"), "nettyClient")
@@ -40,22 +49,48 @@ public class netty_client {
                 //是否开启Nagle，即高实时性（true）减少发送次数（false）
                 .option(ChannelOption.TCP_NODELAY, true);
 
-        connect(bootstrap,IMconfig.host,IMconfig.port);
+        //启动连接
+        connect(bootstrap,IMconfig.host,IMconfig.port,MAX_RETRY);
 
     }
     //建立连接
-    public static void connect (Bootstrap bootstrap,String host,int port){
+    public static void connect (Bootstrap bootstrap,String host,int port,int retry)
+    {
         bootstrap
                 .connect(host, port)
                 .addListener(future -> {
                     if (future.isSuccess()) {
                         System.out.println("连接成功");
-                    } else {
-                        System.out.println("连接失败");
+                    }
+                    else {
                         //这里应该要有个随机退避算法
-                        connect(bootstrap,IMconfig.host,IMconfig.port);
+                        if (retry == 0) {
+                            System.err.println("重试次数已用完，放弃连接！");
+                            return;
+                        }
+                        // 第几次重连
+                        int order = (MAX_RETRY - retry) + 1;
+                        // 此次重连的间隔时间
+                        int delay = 1 << order;
+                        System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                        //使用计划任务来实现退避重连算法
+                        bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1)
+                                                            , delay
+                                                            , TimeUnit.SECONDS);
+                                                            //上面这句的第一个参数的等效语句是
+                                                            /*
+                                                                new Runnable() {
+                                                                    @Override
+                                                                    public void run() {
+                                                                        connect(bootstrap, host, port, retry - 1);
+                                                                    }
+                                                                }
+                                                            */
+
                     }
                 });
+
+
     }
 
 
